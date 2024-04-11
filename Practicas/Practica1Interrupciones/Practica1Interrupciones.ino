6da62ca758a114bbfaba8d0bb7a4adfc0f507efd
Julián Cifuentes Vásquez <juliancifuentesvasquez@gmail.com>
2024-04-11 18:16:42 -0500

    Practica 1 finished

--- Practicas/Practica1Interrupciones/Practica1Interrupciones.ino
+++ Practicas/Practica1Interrupciones/Practica1Interrupciones.ino
@@ -10,31 +10,64 @@
 
 */
 
-#include 
+#include <avr/sleep.h>
 
 #define sensorPin A0
 #define ledPin 10
 #define buttonPin 2
+#define state 13
 
 void sensorFunct();
 void sleepFunct();
 void interruptFunct();
 
 void setup(){
+  Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(state, OUTPUT);
+
+  digitalWrite(state, HIGH);
 
   attachInterrupt(digitalPinToInterrupt(buttonPin), interruptFunct, LOW);
 }
 
+void loop() {
+  sleepFunct();
+}
+
+
+
 void sensorFunct(){
+  int temp = analogRead(sensorPin);
+
+  float voltage = temp * 5.0 / 1023.0;
+  float tempC = voltage * 100.0;
+
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.println(" °C");
 
 }
 
 void sleepFunct(){
+  Serial.println("Entering deep sleep mode...");
   
+  delay(750);
+  digitalWrite(state, LOW);
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  sleep_mode();
+
+  digitalWrite(state, HIGH);
+
 }
 
 void interruptFunct(){
-  
+  sleep_disable();
+
+  digitalWrite(ledPin, HIGH);
+  sensorFunct();
+  digitalWrite(ledPin, LOW);
 }
